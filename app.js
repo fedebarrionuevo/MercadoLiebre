17508d5bb75bbd307a8c6212a9b3021f47a7dd8a
fedebarrionuevo <fede.barrionuevo@hotmail.com>
2021-11-12 20:25:18 -0300

    arreglado partials

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const path = require('path');
 const rutasLogin = require('./routes/users');
 const rutasMain = require('./routes/main')
 const app = express();
+const methodOverride = require("method-override");
+const cors = require("cors");
+
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 app.use('/', rutasLogin, rutasMain)
 
